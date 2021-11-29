7035ab95037f17607de1df1db5771ca8433784fe
Michael Wagner <mwagn4@gmail.com>
2021-11-29 22:51:42 +0100

    WIP commit

--- rootfs/run/01-backup.sh
+++ rootfs/run/01-backup.sh
@@ -7,11 +7,32 @@ function main {
     || perror 3 'backup target not available'
 
   mkdir -p /mnt
-  mount "$BACKUP_TARGET" /mnt || return 4
+  mount "$BACKUP_TARGET" /mnt \
+    || perror 4 'could not mount target device'
 
   if [ ! -e "/mnt/$BACKUP_FILE" ]; then
-    dd if="$BACKUP_SRCDEV" of="/mnt/$BACKUP_FILE"
+    dd if="$BACKUP_SOURCE" of="/mnt/$BACKUP_FILE"
   fi
 
   umount /mnt
+}
+
+function await {
+  file="$1"
+  timeout="$2"
+
+  until [ -e "$file" ]; do
+    sleep 1
+    timeout=$((timeout - 1))
+    if [ $timeout -lt 0 ]; then
+      return 1
+    fi
+  done
+
+  return 0
+}
+
+function perror {
+  echo $2
+  exit $1
 }
